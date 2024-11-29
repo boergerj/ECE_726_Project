c55bb6e2f375abb6f5ac751d483683f62bb2bf45
BoergerJ <jb@boergerfamily.com>
2024-11-29 17:18:37 -0600

    Update Project.m

--- Project.m
+++ Project.m
@@ -1,21 +1,21 @@
 clc; clear; close all;
 
 %% Parameters
-m0 = 2; % Mass of cart
-m1 = .75; % Mass of first linkage
-m2 = 1; % Mass of second linkage
+m0 = 1.5; % Mass of cart
+m1 = .5; % Mass of first linkage
+m2 = .75; % Mass of second linkage
 
-l1 = .75; % Length of linkage 1
-l2 = 1; % Length of linkage 2
+l1 = .5; % Length of linkage 1
+l2 = .75; % Length of linkage 2
 
 %% Linearized Plant
 
 g = 9.8;
 
 M_0 = [...
-    m0+m1+m2 .5*m2*l2 .5*m2*l2;
-    .5*m2*l2 (1/3)*(m1+m2)*l1^2 .5*m2*l1*l2;
-    .5*m2*l2 .5*m2*l1*l2 (1/3)*m2*l2^2;
+    m0+m1+m2 (m1/2+m2)*l1 m2*l2/2;
+    (m1/2+m2)*l1 (m1/3+m2)*l1^2 m2*l1*l2/2;
+    m2*l2/2 m2*l1*l2/2 (m2*l2^2)/3;
     ];
 
 pG_0 = [...
@@ -32,7 +32,7 @@ A = [...
     ];
 
 B = [...
-    zeros(3);
+    zeros(3,1);
     inv(M_0)*H;
     ];
 
@@ -40,7 +40,15 @@ C = eye(6);
 
 %% LQG
 
+% Weight matricies for noise
+W1 = eye(6);
+W2 = eye(6)*2;
 
+% Associated Hamiltonian Matrix
+H_kal = [...
+    A'   C'*inv(W2)*C;
+    W1   -A;
+    ];
 
 
 
