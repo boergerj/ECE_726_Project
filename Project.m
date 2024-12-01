687b08b0c19f2c8ce48b9201c157dbbd90197fbf
BoergerJ <jb@boergerfamily.com>
2024-11-30 18:57:21 -0600

    Update Project.m

--- Project.m
+++ Project.m
@@ -1,6 +1,6 @@
 clc; clear; close all;
 
-%% Parameters
+%% Plant Parameters
 m0 = 1.5; % Mass of cart
 m1 = .5; % Mass of first linkage
 m2 = .75; % Mass of second linkage
@@ -20,8 +20,8 @@ M_0 = [...
 
 pG_0 = [...
     0 0 0;
-    0 ((.5*m1)+m2)*l1*g 0;
-    0 0 .5*m2*l2*g;
+    0 -((.5*m1)+m2)*l1*g 0;
+    0 0 -.5*m2*l2*g;
     ];
 
 H = [1 0 0]';
@@ -40,17 +40,44 @@ C = eye(6);
 
 %% LQG
 
+Q = eye(6);
+
+R = 1;
+
 % Weight matricies for noise
 W1 = eye(6);
 W2 = eye(6)*2;
 
-% Associated Hamiltonian Matrix
-H_kal = [...
-    A'   C'*inv(W2)*C;
-    W1   -A;
+v1 = awgn(zeros(6), 1);
+v2 = awgn(zeros(6), 1);
+
+[~, K] = lqr(A, B, Q, R);
+F = inv(R)*B'*K;
+
+S = lqr(A', C', W1, W2);
+
+A_n = [...
+    A    -B*F;
+    S*C  A-B*F-S*C;
     ];
 
+B_n = [...
+    v1;
+    S*v2;
+    ];
 
+C_n = eye(12);
 
+ic = [0; deg2rad(10); deg2rad(-10); 0; 0; 0; zeros(6,1)];
 
+t = 0:.1:10;
+lqgSys = ss(A_n, B_n, C_n, zeros(12,6));
+kalResponse = initial(lqgSys, ic, t);
 
+figure
+plot(t, real(kalResponse(:,1)))
+hold on
+plot(t, real(kalResponse(:,2)))
+plot(t, real(kalResponse(:,3)))
+xlabel('Time');
+ylabel('Position');
